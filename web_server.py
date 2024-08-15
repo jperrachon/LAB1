e48e2e82b5709e407f160b60c7c67a2b9f4830d0
jperachon <jperrachon@correo.um.edu.uy>
2024-08-15 08:24:31 -0300

    funciona

--- web_server.py
+++ web_server.py
@@ -1,30 +1,28 @@
 from socket import *
+import sys
 
 serverSocket = socket(AF_INET, SOCK_STREAM)
-serverSocket.bind(('', 6789))  # Replace 6789 with your desired port number
+serverSocket.bind(('', 6789))
 serverSocket.listen(1)
 
-connectionSocket, addr = serverSocket.accept()
+while True:
+    print('Ready to serve...')
+    connectionSocket, addr = serverSocket.accept()
+    try:
+        message = connectionSocket.recv(1024)
+        filename = message.split()[1]
+        f = open(filename[1:], 'r')
+        outputdata = f.read()
+        f.close()
+        header = 'HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n'
+        connectionSocket.send(header.encode())
+        connectionSocket.send(outputdata.encode())  # Send the HTML content as encoded bytes
+        connectionSocket.close()
+    except IOError:
+        connectionSocket.send(b'HTTP/1.1 404 Not Found\r\n\r\n')
+        connectionSocket.send(b'<html><head></head><body><h1>404 Not Found</h1></body></html>')
+        connectionSocket.close()
 
-message = connectionSocket.recv(1024).decode()
-
-try:
-    filename = message.split()
-    f = open(filename[1:])
-    outputdata = f.read()
     
-    # Send HTTP header
-    connectionSocket.send("HTTP/1.1 200 OK\r\n\r\n".encode())
-    
-    # Send the content of the requested file[^9^][9]
-    for i in range(0, len(outputdata)):
-        connectionSocket.send(outputdata[i].encode())
-        connectionSocket.send("\r\n".encode())
-    
-    connectionSocket.close()
-except IOError:
-    # Send 404 Not Found response
-    connectionSocket.send("HTTP/1.1 404 Not Found\r\n\r\n".encode())
-    connectionSocket.close()
-
 serverSocket.close()
+sys.exit()
